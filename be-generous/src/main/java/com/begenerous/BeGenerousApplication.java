6d07bf5675cbe8d63603f394348cde1b98f3ef7b
Dávid-Attila <k.david-attila@accenture.com>
2021-10-13 16:33:42 +0300

    environment set up finished

---
+++ be-generous/src/main/java/com/begenerous/BeGenerousApplication.java
@@ -0,0 +1,13 @@
+package com.begenerous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeGenerousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeGenerousApplication.class, args);
+	}
+
+}
